5de4c40728df6b4d154d1f8d119e1f3d8c32d288
edwin-do <doedwin123@gmail.com>
2022-04-08 16:07:00 -0400

    add healthbar and draft YeeHaws

---
+++ farming-2077/Assets/init.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class init : MonoBehaviour
+{
+    // Placed in first run script
+	public static class Globals
+	{
+	       public static int YeeHaws = 0;
+	}
+	
+}
