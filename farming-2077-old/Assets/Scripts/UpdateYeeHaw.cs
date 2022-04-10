213accf4e2df2725a8b1e6ed14a31413e12793a4
edwin-do <doedwin123@gmail.com>
2022-04-09 22:01:12 -0400

    create v2 with new interface

---
+++ farming-2077-old/Assets/Scripts/UpdateYeeHaw.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateYeeHaw : MonoBehaviour
+{
+    //public Globals globals;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print(globals.YeeHaws);
+    }
+}
