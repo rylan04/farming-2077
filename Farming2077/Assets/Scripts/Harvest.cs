99a76b79643e0ffaddbe818ee27740ac22de4602
edwin-do <doedwin123@gmail.com>
2022-04-12 15:37:17 -0400

    fix hix harvest

--- Farming2077/Assets/Scripts/Harvest.cs
+++ Farming2077/Assets/Scripts/Harvest.cs
@@ -6,9 +6,12 @@ using UnityEngine.UI;
 public class Harvest : MonoBehaviour
 {
     public Button harvestBtn;
+    public Text numOfYeeHaws;
 
     public void HarvestCoin() {
         Globals.YeeHaws += 25;
+        string yeehaws = Globals.YeeHaws.ToString();
+        numOfYeeHaws.text = yeehaws;
     }
 
     // Start is called before the first frame update
