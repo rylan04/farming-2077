e5538a4b162b8f1f7f7dd75a25f10ddfb194a5e8
edwin-do <doedwin123@gmail.com>
2022-04-13 01:28:16 -0400

    add hinderers

--- Farming2077/Assets/Scripts/Globals.cs
+++ Farming2077/Assets/Scripts/Globals.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class Globals : MonoBehaviour
 {
     public static int YeeHaws;
-    public static int AnimalHealth;
+    public static float AnimalHealth;
     public static float XP;
     public static int CurrentAnimal;
+    public static bool hasStatus;
+    public static string AnimalStatus;
     // Maps the item name (e.g. "Eggs") to its quantity
     public static Dictionary<string, int> Inventory;
 
     void Start()
     {
         YeeHaws       = 100;
-        AnimalHealth  = 100;
+        AnimalHealth  = 1.0f;
+        hasStatus     = false;
+        AnimalStatus  = "";
         XP            = 0.0f;
         CurrentAnimal = 0;
         Inventory     = new Dictionary<string, int>();
--- Farming2077/Assets/Scripts/Hinderers.cs
+++ Farming2077/Assets/Scripts/Hinderers.cs
@@ -1,49 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hinderers : MonoBehaviour
 {
-    public float nextTime;
-    public float modifier;
+    public Text message;
+    public Image health;
+    // public float nextTime;
+    // public float modifier;
 
     public void SelectHinderer() {
-        int hindererSelector = Random.Range(1, 2);
-        switch (hindererSelector) {
-            case 1: 
-                Globals.YeeHaws -= 10;
-                Globals.XP -= 0.05f;
-                break;
-            case 2: 
-                if (Globals.Inventory["pizza"] > 3) {
-                    Globals.Inventory["pizza"] -= 3;
-                }
-                if (Globals.Inventory["strawberry"] > 2) {
-                    Globals.Inventory["strawberry"] -= 2;
-                }
-                if (Globals.Inventory["sushi"] > 3) {
-                    Globals.Inventory["sushi"] -= 3;
-                }
-                break;
-            case 3: 
-                Globals.AnimalHealth -= 5;
-                break;
-        }
+    	// print("selecting hinderer");
+        // int hindererSelector = Random.Range(1, 2);
+        // switch (hindererSelector) {
+        //     case 1: 
+        //         Globals.YeeHaws -= 10;
+        //         Globals.XP -= 0.05f;
+        //         break;
+        //     case 2: 
+        //         if (Globals.Inventory["pizza"] > 3) {
+        //             Globals.Inventory["pizza"] -= 3;
+        //         }
+        //         if (Globals.Inventory["strawberry"] > 2) {
+        //             Globals.Inventory["strawberry"] -= 2;
+        //         }
+        //         if (Globals.Inventory["sushi"] > 3) {
+        //             Globals.Inventory["sushi"] -= 3;
+        //         }
+        //         break;
+        //     case 3: 
+        //         Globals.AnimalHealth -= 5;
+        //         break;
+        // }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        nextTime = 0f;
+        // nextTime = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        modifier = 3f;
-        nextTime = Time.time + modifier;
-        if (Time.time > nextTime) {
-            SelectHinderer();
+        int RandomStatus = Random.Range(0,2000);
+        // print(RandomStatus.ToString());
+        // print(Globals.hasStatus);
+        
+        if (RandomStatus == 3 && !Globals.hasStatus){
+            Globals.YeeHaws -= 10;
+            Globals.XP -= 0.05f;
+            Globals.hasStatus = true;
+            Globals.AnimalStatus = "Oh no! It's hungry! You just lost some XP and YeeHaws!";
+        }else if (RandomStatus == 5 && !Globals.hasStatus ){
+            Globals.AnimalHealth -= 0.5f;
+            Globals.hasStatus = true;
+            Globals.AnimalStatus = "Oh no! Your specimen got sick and lost health!";
         }
+
+        if (Globals.hasStatus == false){
+            Globals.AnimalStatus = "Your specimen is looking healthy...";
+        }
+
+        message.text = Globals.AnimalStatus;
+        health.fillAmount = Globals.AnimalHealth;
+        // modifier = 3f;
+        // nextTime = Time.time + modifier;
+        // if (Time.time > nextTime) {
+        //     SelectHinderer();
+        // }
     }
 }
