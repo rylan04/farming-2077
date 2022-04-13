193d2e0c15e13a0dd136f2dbd9e0d390bf0b112f
edwin-do <doedwin123@gmail.com>
2022-04-12 20:12:56 -0400

    fix harvest

--- Farming2077/Assets/Scripts/Harvest.cs
+++ Farming2077/Assets/Scripts/Harvest.cs
@@ -8,16 +8,17 @@ public class Harvest : MonoBehaviour
     public Button harvestBtn;
     public Text numOfYeeHaws;
     public bool harvestAvailable;
+    public float timePassed;
 
     public void HarvestCoin() {
         if (Globals.CurrentAnimal == 0) {
-            Globals.YeeHaws += 10;
+            Globals.YeeHaws += 5;
         } else if (Globals.CurrentAnimal == 1) {
-            Globals.YeeHaws += 15;
+            Globals.YeeHaws += 10;
         } else if (Globals.CurrentAnimal == 2) {
-            Globals.YeeHaws += 25;
+            Globals.YeeHaws += 15;
         }
-        harvestAvailable = false;
+        timePassed = 0;
     }
 
     // Start is called before the first frame update
@@ -25,22 +26,16 @@ public class Harvest : MonoBehaviour
     {
         harvestAvailable = false;
         harvestBtn.interactable = false;
-        StartCoroutine(ActivateButton());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!harvestAvailable) {
-            harvestBtn.interactable = false;
+        timePassed += 1 * Time.deltaTime;
+        if (timePassed > 3){
+        	harvestBtn.interactable = true;
+        }else{
+        	harvestBtn.interactable = false;
         }
-        string yeehaws = Globals.YeeHaws.ToString();
-        numOfYeeHaws.text = yeehaws;
-    }
-
-    IEnumerator ActivateButton() {
-        yield return new WaitForSeconds(3f);
-        harvestAvailable = true;
-        harvestBtn.interactable = true;
     }
 }
